3a60ceeb1af94fa62d963682508ca552edcaff10
Abhay sagar kargeti <abhaykargeti2001@gmail.com>
2025-04-07 14:51:21 +0530

    Add global CORS config to allow local frontend access

--- src/main/java/com/location/controller/LocationController.java
+++ src/main/java/com/location/controller/LocationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/locations")
 public class LocationController {
